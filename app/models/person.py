69f414bf8125256e041596c716c5946ccb793696
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-16 23:33:56 +0700

    Add shirt_size to Person

--- app/models/person.py
+++ app/models/person.py
@@ -2,5 +2,8 @@ from django.db.models import Model, CharField
 
 
 class Person(Model):
+    SHIRT_SIZES = {"S": "Small", "M": "Medium", "L": "Large"}
+
     first_name = CharField(max_length=30)
     last_name = CharField(max_length=30)
+    shirt_size = CharField(max_length=1, choices=SHIRT_SIZES, default="S")
