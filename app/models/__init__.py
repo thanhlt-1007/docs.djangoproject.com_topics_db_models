a91052ba05c18e41422e742f0efc055d90e21d38
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-17 11:36:43 +0700

    Add Manufacturer model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .musician import Musician  # noqa
 from .album import Album  # noqa
 from .runner import Runner  # noqa
 from .fruit import Fruit  # noqa
+from .manufacturer import Manufacturer  # noqa
