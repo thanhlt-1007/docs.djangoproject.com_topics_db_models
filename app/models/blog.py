f9e06d6ce7699d8299eaa46f03fca113f37a2d43
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-18 00:42:25 +0700

    Add save method to Blog

--- app/models/blog.py
+++ app/models/blog.py
@@ -4,3 +4,9 @@ from django.db.models import Model, CharField, TextField
 class Blog(Model):
     name = CharField(max_length=100)
     tagline = TextField()
+
+    def save(self, **kwargs):
+        if self.name == "Yoko Ono's blog":
+            return
+        else:
+            super().save(**kwargs)
